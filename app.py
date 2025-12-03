8ac65dc040cda625eb89e982cc60bf4e772507ca
azaria-fairuz <fairuz.akbar.azaria@gmail.com>
2025-12-03 13:58:59 +0700

    [feat] added return statement and increased max file transfer

--- app.py
+++ app.py
@@ -8,22 +8,30 @@ from concurrent import futures
 
 class FrameServiceServicer(frame_pb2_grpc.FrameServiceServicer):
     def SendFrame(self, request, context):
+        print("[NEW] request has been accpeted")
         width = request.width
         height = request.height
         frame_bytes = request.data
+        size_in_bytes = len(frame_bytes)
 
-        frame = np.frombuffer(frame_bytes, dtype=np.uint8).reshape(height, width, 3)
-        print(f"recieved frame with width {width} and height {height}")
+        print(f"[RECEIVED] Frame: {width}x{height}, {size_in_bytes / 1024:.2f} KB")
 
-        # add sending logic in here to detection
+        return frame_pb2.Empty()
     
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=[
+            ('grpc.max_send_message_length', 50 * 1024 * 1024),
+            ('grpc.max_receive_message_length', 50 * 1024 * 1024)
+        ]
+    )
+
     frame_pb2_grpc.add_FrameServiceServicer_to_server(FrameServiceServicer(), server)
     server.add_insecure_port('[::]:8500')
-
     server.start()
-    print("listening on port 8500")
+
+    print("[START] Listening on port 8500")
     server.wait_for_termination()
 
 if __name__ == "__main__":
